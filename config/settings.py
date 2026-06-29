7d68595574507594de28c8ea0c3ec1861c716145
buladou <kellyhu12@qq.com>
2026-06-29 16:29:42 +0800

    chore(config): remove session authentication from default auth classes
    drop SessionAuthentication from REST_FRAMEWORK default auth classes to enforce JWT-only auth by default

--- config/settings.py
+++ config/settings.py
@@ -150,7 +150,6 @@ CORS_ALLOW_ALL_ORIGINS = DEBUG  # 开发模式允许所有来源（支持 192.16
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
